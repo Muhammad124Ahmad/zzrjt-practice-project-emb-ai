fd1ffa5dca07cd061b1376fba79e501cd70875ff
muhammad1249ahmad <>
2025-08-01 20:21:23 +0500

    Applied Static Code Analysis TO server.py

--- server.py
+++ server.py
@@ -12,13 +12,16 @@ app = Flask(__name__)
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
+    """This Function Retrieves text from html and
+    applies sentimental analysis and returns a string"""
     text = request.args.get("textToAnalyze")
     result = sentiment_analyzer(text)
-    return (f"The given text has been identified as {result['label']} with a score of {result['score']}")
+    return f"This is{result['label']} sentence with score of {result['score']}"
 
 
 @app.route("/")
 def render_index_page():
+    """This Function renders index.html template"""
     return render_template("index.html")
 
 
